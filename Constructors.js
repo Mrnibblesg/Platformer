1f773011b912eaf8ca5ccb3779ae2b94a49fd12e
Mrnibblesg <mrnibblesgutierrez@gmail.com>
2018-01-10 19:48:01 -0800

    Test
    Figure out how to update files on github

--- Constructors.js
+++ Constructors.js
@@ -3,4 +3,11 @@ function Platform(x,y,w,h = 2 * w){
 	this.y = y;
 	this.w = w;
 	this.h = h;
+	
+	this.draw = function(){
+		C.beginPath();
+		C.fillStyle = "black";
+		C.fillRect(this.x,this.y,this.w,this.h);
+		C.closePath();
+	}
 }

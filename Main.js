cec0bba5396b3c251b28d782f7b5687213d8df31
Mrnibblesg <mrnibblesgutierrez@gmail.com>
2018-01-11 21:22:00 -0800

    Includes event listeners and game loop

--- Main.js
+++ Main.js
@@ -2,10 +2,30 @@ const canvas = document.getElementById("myCanvas");
 const c = canvas.getContext("2d");
 const W = window.innerWidth;
 const H = window.innerHeight;
+let keysDown = [];
 canvas.width = W;
 canvas.height = H;
+
+let game = {};
+
+addEventListener("keydown",function(e){
+	keysDown[e.keyCode] = true;
+	keyPressed(e);
+});
+addEventListener("keyup",function(e){
+	delete keysDown[e.keyCode];
+});
+
 
 let globalX = 0;
 let globalY = 0;
 
 initialize();
+
+function gameLoop(){
+	c.clearRect(0,0,W,H);
+	whileKeyPressed();
+	game.draw();
+	game.update();
+	requestAnimationFrame(gameLoop);
+}

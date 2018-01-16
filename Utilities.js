78c0515526259608c85b8bec04c6710f95fcad3b
Mrnibblesg <mrnibblesgutierrez@gmail.com>
2018-01-15 21:09:55 -0800

    Added methods and some documentation
    Added getDistance, and logistic (sigmoid function method), currently used in calculations in the screen.follow method.

--- Utilities.js
+++ Utilities.js
@@ -1,6 +1,32 @@
-let drawRect = function(x,y,w,h,col = "black"){
+/**
+ *@fileOverview Extra functions that I did not want to put into any of the other files live here.
+ */
+ 
+/**
+ * The most basic draw function, but with added screen offsets.
+ * BACKGROUNDS SHOULD NOT USE THIS!
+ */
+function drawRect(x,y,w,h,col,stroke){
 	c.beginPath();
 	c.fillStyle = col;
-	c.fillRect(x,y,w,h);
+	c.strokeStyle = stroke;
+	c.rect(x + screen.x,y + screen.y,w,h);
+	c.fill();
+	c.stroke();
 	c.closePath();
+}
+
+/**
+* Gets the distance from 1 point to another.
+*/
+function getDistance(x1,y1,x2,y2){
+	let xOff = x1 - x2;
+	let yOff = y1 - y2;
+	return Math.sqrt(xOff * xOff + yOff * yOff);
+}
+
+function logistic(max,steepnessCoefficient,x,xOffset){
+	let exponent = x - xOffset;
+	let denominator = 1 + Math.pow(steepnessCoefficient,exponent);
+	return max / denominator;
 }

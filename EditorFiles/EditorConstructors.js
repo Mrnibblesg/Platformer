337686018515423400a0c50ba17336b8d365c005
Mrnibblesg <mrnibblesgutierrez@gmail.com>
2018-02-01 20:51:03 -0800

    InfoBox Button, platFrame added
    Added clickable buttons to the InfoBox, and an object called platFrame for help with creating platforms

--- EditorFiles/EditorConstructors.js
+++ EditorFiles/EditorConstructors.js
@@ -1,68 +1,132 @@
 "use strict";
-function InfoBox(x,y,w,h,lines = []){
+
+/**
+ * The InfoBox class is a container for information so that you know the controls
+ * and know how to use the editor.
+ */
+function InfoBox(x,y,w,h,header,lines,buttons = []){
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
 	
+	this.header = header;
+	
 	this.lines = lines;
+	this.buttons = buttons;
 	
 	this.minimized = false;
 	
-	this.buttons;
+	this.buttons = buttons;
 	
 	this.draw = function(){
 		if (this.minimized){
 			//Draw maximize button only
 			drawRect(this.x,this.y,20,20,"green","black",true);
-			c.beginPath();
-			c.fillStyle = "white";
-			c.font = "30px Verdana";
-			c.textAlign = "center";
-			c.textBaseline = "middle";
-			c.fillText("+",this.x + 10,this.y + 8);
-			c.closePath();
+			drawText("+",this.x + 10,this.y + 8, "20px Verdana","center","middle","white");
+			
 		}
 		else{
+			//Draw box, then contents
+			
+			
 			//Draw box
 			drawRect(this.x,this.y,this.w,this.h,"silver","black",true);
 			
 			//Draw minimize button
 			drawRect(this.x,this.y,20,20,"red","black",true);
-			c.beginPath();
-			c.fillStyle = "white";
-			c.font = "30px Verdana";
-			c.textAlign = "center";
-			c.textBaseline = "middle";
-			c.fillText("-",this.x + 10,this.y + 8);
-			c.closePath();
+			drawText("-", this.x + 10, this.y + 8, "30px Verdana", "center", "middle", "white");
 			
-			//Draw text
+			//Draw text and buttons
 			for (let i = 0; i < this.lines.length; i++){
-				c.beginPath();
-				c.fillStyle = "black";
-				c.font = "15px Verdana";
-				c.textAlign = "start";
-				c.textBaseline = "hanging";
-				c.fillText(this.lines[i],this.x + 10, i * 20);
-				c.closePath();
+				
+				let contents = this.lines[i];
+				let x = this.x + 10;
+				let y = this.y + (i + 1) * 20 + 10;
+				
+				if (typeof(contents) === "string"){
+					drawText(contents,x,y,"15px Verdana", "start", "hanging", "black");
+					
+				}
+			}
+			
+			//Draw the header
+			drawRect(this.x + 20,this.y,this.w - 20,20,"gray","black",true);
+			drawText(this.header, this.x + (this.w - 20) / 2 + 20, this.y + 10, "15px Verdana", "center","middle","white");
+			
+			
+			for (let button of this.buttons){
+				button.draw(this.x,this.y);
 			}
 		}
 	}
 	
-	this.minimize = function(){
-		this.minimized = true;
-	}
-	this.maximize = function(){
-		this.minimized = false;
+	this.toggleSize = function(){
+		this.minimized = !this.minimized;
 	}
 }
-function button(x,y,w,h,text,func){
-	this.x = x;
-	this.y = y;
+
+/**
+ * The button is a clickable item that goes in an infobox's lines property.
+ * when clicked, it executes its function.
+ */
+function Button(relX,relY,w,h,text,func){
+	this.relX = relX;
+	this.relY = relY;
 	this.w = w;
 	this.h = h;
 	this.text = text;
 	this.execute = func;
 	
+	this.draw = function(infoBoxX,infoBoxY){
+		let totalX = infoBoxX + this.relX;
+		let totalY = infoBoxY + this.relY;
+		
+		//Draw box, then text
+		drawRect(totalX,totalY,this.w,this.h,"green","black",true);
+		drawText(this.text,totalX + 5,totalY + 5, "15px Verdana", "start", "hanging", "black");
+	}
+}
+let platFrame = {
+	x1: undefined,
+	y1: undefined,
+	x2: undefined,
+	y2: undefined,
+	
+	corner1: false,
+	corner2: false,
+	creating: false,
+	
+	
+	draw: function(){
+		if (this.creating){
+			if (this.corner1 && this.corner2){
+				let w = this.x2 - this.x1;
+				let h = this.y2 - this.y1;
+				drawRect(this.x1,this.y1,w,h,"transparent","red",false);
+			}
+			else if (this.corner1){
+				drawCircle(this.x1,this.y1,2,"red");
+			}
+		}
+	},
+	
+	create: function(){
+		console.log("creating");
+		let w = this.x2 - this.x1;
+		let h = this.y2 - this.y1;
+		let newPlatform = new Platform(this.x1,this.y1,w,h);
+		this.reset();
+		return newPlatform;
+	},
+	
+	reset: function(){
+		this.creating = false;
+		this.x1 = undefined;
+		this.x2 = undefined;
+		this.y1 = undefined;
+		this.y2 = undefined;
+		this.corner1 = false;
+		this.corner2 = false;
+	}
 }
